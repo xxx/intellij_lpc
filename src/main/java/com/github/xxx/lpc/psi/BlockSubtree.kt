763d28a4fbe9d6564a9e2c2e6e8eb05d18be7389
mpd <mpd@jesters-court.net>
2019-12-26 17:41:42 -0800

    add a shitty hack to BlockSubtree so recursive function calls are seen as references

--- src/main/java/com/github/xxx/lpc/psi/BlockSubtree.kt
+++ src/main/java/com/github/xxx/lpc/psi/BlockSubtree.kt
@@ -4,6 +4,7 @@ import com.github.xxx.lpc.LPCLanguage
 import com.intellij.lang.ASTNode
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNamedElement
+import com.intellij.psi.util.PsiTreeUtil
 import org.antlr.intellij.adaptor.SymtabUtils
 import org.antlr.intellij.adaptor.psi.ANTLRPsiNode
 import org.antlr.intellij.adaptor.psi.ScopeNode
@@ -12,10 +13,19 @@ class BlockSubtree(node: ASTNode) : ANTLRPsiNode(node), ScopeNode {
     override fun resolve(element: PsiNamedElement): PsiElement? {
         //		System.out.println(getClass().getSimpleName()+
         //		                   ".resolve("+element.getName()+
-        //		                   " at "+Integer.toHexString(element.hashCode())+")");
-        return SymtabUtils.resolve(
+        //		                   " at "+Integer.toHexString(element.hashCode())+")");\
+        val node = SymtabUtils.resolve(
             this, LPCLanguage.INSTANCE,
             element, "/block//local_declare_statement//new_local_name"
         )
+
+        // This is an obnoxious hack because of the grammar.
+        // We have both prototypes and implementations to have to deal with, and it's nice to be able to factor
+        // the duplication into a single rule. It just breaks right here though.
+        if (node is FunctionDeclSubtree) {
+            return PsiTreeUtil.getParentOfType(element, FunctionImplementationSubtree::class.java)
+        }
+
+        return node
     }
 }
