d5a8d56a360c2213efd754a9ed4fcafa10d460cb
mpd <mpd@jesters-court.net>
2019-12-28 13:41:31 -0800

    stub getVariants

--- src/main/java/com/github/xxx/lpc/psi/FunctionRef.kt
+++ src/main/java/com/github/xxx/lpc/psi/FunctionRef.kt
@@ -1,6 +1,9 @@
 package com.github.xxx.lpc.psi
 
+import com.github.xxx.lpc.LPCLanguage
 import com.intellij.psi.PsiElement
+import com.intellij.util.containers.toArray
+import org.antlr.intellij.adaptor.xpath.XPath
 
 /** A reference object associated with (referring to) a IdentifierPSINode
  * underneath a CallSubtree root.
@@ -9,4 +12,11 @@ class FunctionRef(element: IdentifierPSINode) : LPCElementRef(element) {
     override fun isDefSubtree(def: PsiElement?): Boolean {
         return def is FunctionImplementationSubtree
     }
+
+    // override fun getVariants(): Array<Any> {
+    //     val collection = XPath.findAll(LPCLanguage.INSTANCE, element.containingFile,
+    //         "//function_implementation/Identifier").filterNotNull()
+    //
+    //     return collection.toArray(arrayOf<Any>(collection.size))
+    // }
 }
